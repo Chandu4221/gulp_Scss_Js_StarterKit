d281d6d76d31df817410f0289f22043be734c20b
chandraShekhar <chndraja@gmail.com>
2018-02-13 13:16:05 +0530

    scss linting and source maps added

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,35 @@ var sass        = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var sassLint = require('gulp-sass-lint');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass-lint'], function() {
     browserSync.init({
         server: "./app"
     });
-    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/scss/*.scss", ['sass-lint']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
     gulp.watch("app/js/*.js").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/**/*.scss")
-        .pipe(sass().on('error',sass.logError))
-        .pipe(gulp.dest("app/css"))
-        .pipe(sourcemaps.init())
-        .pipe(cssmin())
-        .pipe(sourcemaps.write())
-        .pipe(rename({
-            suffix:'.min'
-        }))
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.stream());
+// sass-lint check
+gulp.task('sass-lint', function (error) {
+  return gulp.src('app/scss/**/*.s+(a|c)ss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError().on('error',error))
+    .pipe(sass().on('error',sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("app/css"))
+    .pipe(cssmin())
+    .pipe(sourcemaps.write())
+    .pipe(rename({
+        suffix:'.min'
+    }))
+    .pipe(gulp.dest('app/css'));
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
